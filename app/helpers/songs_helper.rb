7614b20a10b10ffbc7b4bc3499c5cd13cfca51e4
sophie <justanothersophie@gmail.com>
2021-09-11 02:44:28 -0400

    commit tree 431687874307602a6044db7c90a67fc68330c7eb

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+    def artist_select(artist_id)
+        if artist_id.nil?
+            select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+        else
+            hidden_field_tag "song[artist_id]", artist_id
+        end
+    end
+    
 end
